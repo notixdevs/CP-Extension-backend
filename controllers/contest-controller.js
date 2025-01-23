09730ff159029a8b4892e901d6c39210147df004
stvy <stvycoding@gmail.com>
2025-01-23 15:58:13 +0530

    fetch data from api

--- controllers/contest-controller.js
+++ controllers/contest-controller.js
@@ -1,5 +1,12 @@
-const getContests = (req, res) => {
-    res.send("Controller works");
-};
+const {fetchContests} = require('../helper-functions/functions');
+
+const getContests = async (req,res) => {
+    try {
+        const contests = await fetchContests();
+        res.json(contests);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+}
 
 module.exports = getContests;
---
+++ helper-functions/functions.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const fetchContests = async () => {
+    const currentDate = new Date();
+    const oneWeekLater = new Date();
+    oneWeekLater.setDate(currentDate.getDate() + 50);
+
+    const startDate = currentDate.toISOString().slice(0, 19);
+    const endDate = oneWeekLater.toISOString().slice(0, 19);
+
+    const apiUrl = `https://clist.by/api/v4/contest/?upcoming=true&start__gte=${startDate}&start__lte=${endDate}&username=${process.env.USER_ID}&api_key=${process.env.API_KEY}&order_by=start`;
+
+    try {
+        const response = await axios.get(apiUrl);
+        return response.data.objects;
+    } catch (error) {
+        console.error('Error fetching contests:', error.message);
+        throw new Error('Could not fetch contests.');
+    }
+};
+
+module.exports = { fetchContests };
