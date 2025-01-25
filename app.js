e219810adc69ab4b07024fb6b78f94ef97b432ba
Shivam <kingshivam2004@gmail.com>
2025-01-25 13:19:26 +0530

    auto health check

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 app.use(express.json());
 
@@ -9,12 +10,31 @@ const PORT = process.env.PORT || 3000;
 const contestRoutes = require('./routes/contest-routes');
 
 app.get('/healthz',(req,res)=>{
+    console.log("Health Check")
     res.send("Healthy connection");
 });
 
 app.use('/api', contestRoutes);
 
 
+const url = `https://cp-extension-backend.onrender.com/healthz`;
+const interval = 300000;
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+    //   console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+
+setInterval(reloadWebsite, interval);
+
+
 app.listen(PORT,()=>{
     console.log(`Server running on ${PORT}`);
 });
+
